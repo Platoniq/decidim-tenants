edc479155a7083feea0c0c97808fbdd8d8eac57d
Ivan Vergés <ivan@platoniq.net>
2020-06-15 23:51:03 +0200

    add delayed job

---
+++ bin/delayed_job_cron.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PATH="\$HOME/.rbenv/bin:\$PATH"
+eval "\$(rbenv init -)"
+APP_PATH=\$(dirname \$(dirname \$(realpath \$0)))
+
+if ! [ -s \$APP_PATH/tmp/pids/delayed_job.pid ]; then
+  RAILS_ENV=production \$APP_PATH/bin/delayed_job start
+fi
