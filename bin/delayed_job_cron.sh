cc3e38f959ab9f4c7fdf246904391aac1339d198
Ivan Vergés <ivan@platoniq.net>
2021-12-12 14:58:39 +0100

    remove delayedjob utils

--- bin/delayed_job_cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-APP_PATH=$(dirname $(dirname $(realpath $0)))
-
-if ! [ -s $APP_PATH/tmp/pids/delayed_job.pid ]; then
-  RAILS_ENV=production $APP_PATH/bin/delayed_job start
-fi
